b8d573be214d4e3482d39b44094400936ab22e56
forDecember <1921567337@qq.com>
2023-10-10 13:12:17 +0800

    reFactory: 重构日志逻辑 - Linux 和 Windows 执行不同编译 - 解决环境不同导致的报错问题

--- logger/logger.go
+++ logger/logger.go
@@ -7,8 +7,6 @@ import (
 	"os"
 	"runtime"
 	"strconv"
-	"strings"
-	"syscall"
 )
 
 const (
@@ -56,47 +54,6 @@ func overridePrintln(l *log.Logger, isDisplay bool, v ...any) {
 	err := l.Output(4, fmt.Sprintln(v...))
 	if err != nil {
 		return
-	}
-}
-
-// windows 操作系统下调用系统api实现cmd着色
-func winColorPrintln(msg string, v ...any) {
-	kernel32 := syscall.NewLazyDLL("kernel32.dll")
-	proc := kernel32.NewProc("SetConsoleTextAttribute")
-	switch strings.ToLower(msg) {
-	case "error":
-		_, _, _ = proc.Call(uintptr(syscall.Stdout), uintptr(4))
-	case "warning":
-		_, _, _ = proc.Call(uintptr(syscall.Stdout), uintptr(6))
-	case "info":
-		_, _, _ = proc.Call(uintptr(syscall.Stdout), uintptr(2))
-	case "debug":
-		_, _, _ = proc.Call(uintptr(syscall.Stdout), uintptr(5))
-	default:
-	}
-
-	myLogger.SetPrefix("[" + msg + "]")
-	overridePrintln(myLogger, logMap[logLevel] >= logMap[msg], v...)
-
-	_, _, _ = proc.Call(uintptr(syscall.Stdout), uintptr(7))
-}
-
-func colorPrint(color string, msg string, v ...any) {
-	if isUnix {
-		myLogger.SetPrefix(color + "[" + msg + "]" + colorReset)
-		overridePrintln(myLogger, logMap[logLevel] >= logMap[msg], color+fmt.Sprint(v...)+colorReset)
-		// 上一行参数传v表示整体当成数组传入，参数传v... 表示多个参数分别传入
-	} else {
-		winColorPrintln(msg, fmt.Sprint(v...))
-	}
-}
-
-func colorPrintf(format string, color string, msg string, v ...any) {
-	if isUnix {
-		myLogger.SetPrefix(color + "[" + msg + "]" + colorReset)
-		overridePrintln(myLogger, logMap[logLevel] >= logMap[msg], color+fmt.Sprintf(format, v...)+colorReset)
-	} else {
-		winColorPrintln(msg, fmt.Sprintf(format, v...))
 	}
 }
 
